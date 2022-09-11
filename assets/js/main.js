1da8a555ffedcc3d5c8419d8cd7513008914efaa
ArmandoHoLa <99104425+ArmandoLazalde@users.noreply.github.com>
2022-09-10 21:03:23 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,3 +177,24 @@ function activeWork() {
 }
 
 linkWork.forEach(l=> l.addEventListener("click", activeWork))
+
+/*================= Work Popup ==========================*/
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("work__button")) {
+        togglePortfolioPopup()
+        portfolioItemDetails(e.target.parentElement);  
+    }      
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+    document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
+
